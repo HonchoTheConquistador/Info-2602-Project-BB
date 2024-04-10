db939985231c9c6993ec402b19db6afb377c3855
Honcho TheConquistador <jehlani.joseph@my.uwi.edu>
2024-04-10 21:56:35 +0000

    Added a gym database from kaagle(will include the link), created the workout model to hold the gym data

---
+++ App/models/workouts.py
@@ -0,0 +1,29 @@
+from App.database import db
+
+Title,Desc,Type,BodyPart,Equipment,Level,Rating,RatingDesc
+
+class Wokrkouts(db.Model):
+    workoutID = db.Column(db.Integer, nullable=False,primary_key=True)
+    workoutName = db.Column(db.String(50),nullable = False)
+    description = dv.Column(db.String, nullable=False)
+    workoutType = db.Column(db.String(30),nullable = False)
+    bodypart = db.Column(db.String(20),nullable = False)
+    Level = db.Column(db.Integer,nullable = False)
+    
+    def __init__(self,workoutID,workoutName,description,workoutType,bodyPart,Level):
+        self.workoutID = workoutID
+        self.workoutName = workoutName
+        self.description = description
+        self.workoutType = workoutType
+        self.bodypart = bodyPart
+        self.Level = Level
+
+    def get_json(self):
+        return{
+            "workoutName" : self.workoutName,
+            "description" : self.description,
+            "workoutType" : self.workoutType,
+            "bodypart" : self.bodypart,
+            "level" : self.level
+
+        }
